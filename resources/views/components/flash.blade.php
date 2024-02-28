92adcdaae8e0dbbc0513d4906eb126ebf4f4c0cb
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-02-28 15:50:15 +0400

    feat: added images changing instantly on upload and deleting functionality, finished project

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -10,7 +10,8 @@
         <p class="ms-3 text-sm font-normal pl-6">{{session('success')}}</p>
         
     </div>
-@elseif(session()->has('deleted'))
+
+@elseif(session()->has('lblbla'))
     <div id="toast-danger" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center  max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow border-b border-b-main-red" role="alert">
         <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-main-red bg-main-red/10 rounded-lg">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -19,4 +20,14 @@
         </div>
         <p class="ms-3 text-sm font-normal pl-6">{{session('deleted')}}</p>
     </div>
+
+@elseif(session()->has("error"))
+<div id="toast-warning" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow border-b border-b-orange-500" role="alert">
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
+        </svg>
+    </div>
+    <p class="ms-3 text-sm font-normal">{{session('error')}}</p>
+</div>
 @endif
--- resources/views/components/form/upload-input.blade.php
+++ resources/views/components/form/upload-input.blade.php
@@ -1,7 +1,7 @@
 @props(['name','text'])
 
 <div>
-    <input id="{{$name}}" name="{{$name}}" type="file" accept="image/*" hidden />
+    <input onchange="changeImage(event,name)" id="{{$name}}" name="{{$name}}" type="file" accept="image/*" hidden />
     <label for="{{$name}}" class="whitespace-nowrap flex items-center gap-10 rounded-xl py-5 px-12 border border-main-blue leading-4 font-bold text-main-blue uppercase focus:outline-none hover:bg-main-blue/10 duration-100 
     @error($name) 
     border-main-red
@@ -15,3 +15,30 @@
             {{$message}}
         </p>   
 @enderror
+
+@once 
+<script>
+   function changeImage(event,name){
+        if(event.target.files && event.target.files[0]){
+            const reader = new FileReader();
+            reader.onload = function(e){
+                document.querySelector(`#img-${name}`).src = e.target.result;
+                document.querySelector(`#delete-${name}`).style.display = 'block';
+
+            }
+            reader.readAsDataURL(event.target.files[0]);
+        }
+   }
+
+   function removeImage(name){
+    if(name === 'profile_image'){
+        document.querySelector(`#img-${name}`).src = "{{auth()->user()->profile_image ?  asset('storage/images/' . auth()->user()->profile_image) : asset('storage/images/basic-avatar.png')}}"
+    }else if(name === "cover_image"){
+        document.querySelector(`#img-${name}`).src = "{{file_exists(public_path('storage/images/cover_image.png')) ? asset('storage/images/cover_image.png') : asset('storage/images/intersect.png')}}"
+    }
+        const fileInput = document.querySelector(`#${name}`).value = "";
+        document.querySelector(`#delete-${name}`).style.display = 'none';
+
+   }
+</script>
+    @endonce
