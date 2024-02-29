53a3889dd40ee11618ad44579bb95d8e78173efc
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-02-29 17:42:12 +0400

    fix: removed comments

--- app/Http/Requests/StoreLoginRequest.php
+++ app/Http/Requests/StoreLoginRequest.php
@@ -8,10 +8,6 @@ use Illuminate\Validation\Rule;
 class StoreLoginRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
--- app/Http/Requests/StoreProfileRequest.php
+++ app/Http/Requests/StoreProfileRequest.php
@@ -7,10 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreProfileRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
