16ee908ac6e00e936d10d553f1ed3a636ba988dc
Tr1ckyy1 <omar.jangavadze11@gmail.com>
2024-03-01 12:47:04 +0400

    fix: added gitkeep so that server has basic images, fixed popup message display never disappearing

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -12,7 +12,9 @@
     </div>
 
 @elseif(session()->has('deleted'))
-    <div id="toast-danger" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center  max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow border-b border-b-main-red" role="alert">
+    <div x-data="{show: true}"
+         x-init="setTimeout(() => show = false,3000)"
+         x-show="show" id="toast-danger" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center  max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow border-b border-b-main-red" role="alert">
         <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-main-red bg-main-red/10 rounded-lg">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
@@ -22,7 +24,9 @@
     </div>
 
 @elseif(session()->has("error"))
-<div id="toast-warning" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow border-b-2 border-b-orange-500" role="alert">
+<div x-data="{show: true}"
+     x-init="setTimeout(() => show = false,3000)"
+     x-show="show"id="toast-warning" class="absolute left-1/2 top-4 -translate-x-1/2 flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow border-b-2 border-b-orange-500" role="alert">
     <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"/>
